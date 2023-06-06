0aad276527226b26d8d32270904b7d4e25cbbe43
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2023-06-06 20:06:35 +0530

    consume counter component from remote-app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.experiments = { layers: true, topLevelAwait: true };
+    config.plugins.push(
+        new NextFederationPlugin({
+          name: 'hostApp',
+          remotes: {
+            // remoteApp: `remoteApp@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+              remoteApp: `remoteApp@https://legendary-creponne-95a0e0.netlify.app//_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          },
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {}
+        })
+    );
+    return config;
+  }
 }
 
 module.exports = nextConfig
