b32a6d177110792a09f94c745ee7f550d24dd740
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2023-06-07 14:02:20 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,13 @@ const Counter = dynamic(() => import("remoteApp/counter"))
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const counterProps: any = {
+        defaultCount: 5
+    }
   return (
       <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
         <p className={'m-0 p-0'}>Host App</p>
-          <Counter defaultCount={5} />
+          <Counter {...counterProps} />
       </main>
   )
 }
